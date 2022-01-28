3366324f0564239acdd963c620026cb14c04c2b5
masudrana <masudranaacb@gmail.com>
2022-01-29 00:05:07 +0600

    functinal frist commit a simple cod

---
+++ function.js
@@ -0,0 +1,13 @@
+//loop is not multiple call system
+//functin is a multiple call system
+
+function study(){
+    console.log("ami akhon vat khabo");
+    console.log("na ami jeteparbona");
+}
+study();
+var a=20;
+var b=40;
+var c=a+b;
+console.log(c);
+study();
