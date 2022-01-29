655013c759249972a6e5bd4fe8264968e8605b5a
masudrana <masudranaacb@gmail.com>
2022-01-29 14:29:12 +0600

    some basic function

--- function.js
+++ function.js
@@ -1,9 +1,23 @@
-function bringSingara(taka){
-    console.log('sigar anar jonno taka sise =',taka);
-    var singPrice=10;
-    var singarQuintity=taka/singPrice;
-    return singarQuintity;
+// additional function
+function addTwoNumber(number1, number2){
+    console.log('enter your two number=', number1,number2);
+    var total=number1+number2;
+    return total;
 }
-var money=250;
-var singara=bringSingara(money);
-console.log('mama now tomar sigara',singara);
+var result= addTwoNumber(10, 20);
+console.log('addition your number =', result);
+
+//subtrction numbers
+function towSubtraction(number1,number2){
+    var subtrectin=number1-number2;
+    return subtrectin;
+}
+var result=towSubtraction(10,20);
+console.log('subtrection your numbers =', result);
+
+function twoNumberMultiplection(number1,number2){
+    var multiplection=number1*number2;
+    return multiplection;
+}
+var result=twoNumberMultiplection(10,20);
+console.log('multipection your tow number =' ,result);
